2f6f8b190711d894ffb6be12988a82987fba662b
Zack Tylor <taylor.zackory@gmail.com>
2017-10-28 19:14:31 -0500

    move app component files

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import TopBar from './TopBar.js';
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <TopBar />
+        <div className="container">khlhlk</div>
+      </div>
+    );
+  }
+}
+
+export default App;
