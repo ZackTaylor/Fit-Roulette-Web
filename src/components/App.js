584bc53c514d50e06d3b2db3b729c561365de371
Zack Tylor <taylor.zackory@gmail.com>
2017-10-30 21:32:28 -0500

    use time container insead of timeer

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import TopBar from './TopBar';
+import TimerContainer from '../containers/TimerContainer';
+
 class App extends Component {
   render() {
     return (
       <div>
         <TopBar />
         <div className="container" />
+        <TimerContainer />
       </div>
     );
   }
