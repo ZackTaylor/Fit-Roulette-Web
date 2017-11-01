fb389f68591fd88b3f4c7bd8a05f8db7a9564514
Zack Tylor <taylor.zackory@gmail.com>
2017-10-31 23:22:31 -0500

    add the ExerciseContainer component

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import TopBar from './TopBar';
 import TimerContainer from '../containers/TimerContainer';
+import ExerciseContainer from '../containers/ExerciseContainer';
 
 class App extends Component {
   render() {
     return (
       <div>
         <TopBar />
-        <div className="container" />
+        <ExerciseContainer />
         <TimerContainer />
       </div>
     );
