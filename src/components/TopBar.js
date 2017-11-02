9f798d49c17b4acdd88f1f623a3706c8eeb79c76
Zack Tylor <taylor.zackory@gmail.com>
2017-11-02 14:32:36 -0500

    change navbar to regular css

--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,23 +1,35 @@
-import React from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import React, { Component } from 'react';
+import './TopBar.css';
 
-const TopBar = () => (
-  <Navbar inverse collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a>Fit Roulette</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} href="#">
-          Take a Spin
-        </NavItem>
-      </Nav>
-      <Nav pullRight />
-    </Navbar.Collapse>
-  </Navbar>
-);
+class TopBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      navClass: 'topnav'
+    };
+  }
+
+  toggleDropdown = () => {
+    this.setState({
+      navClass: this.state.navClass === 'topnav' ? 'topnav responsive' : 'topnav'
+    });
+  };
+
+  render() {
+    return (
+      <ul className={this.state.navClass} id="myTopnav">
+        <li>
+          <a>Fit Roulette</a>
+        </li>
+        <li>
+          <a>Take A Spin</a>
+        </li>
+        <li className="icon">
+          <a onClick={this.toggleDropdown}>☰</a>
+        </li>
+      </ul>
+    );
+  }
+}
 
 export default TopBar;
